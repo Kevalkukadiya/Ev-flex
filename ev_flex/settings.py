a1cc888b49584347c3648f43d547146dadb9e5b7
Kevalkukadiya <kevalkukadiya5490@gmail.com>
2022-09-09 16:58:25 +0530

    State Api

--- ev_flex/settings.py
+++ ev_flex/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'user',
+    'state',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +78,11 @@ WSGI_APPLICATION = 'ev_flex.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':  'ev_flex',
+        'HOST': 'localhost',
+        'USER': 'keval',
+        'PASSWORD': 'KEVAL_1984',
     }
 }
 
